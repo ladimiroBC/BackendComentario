18d6cac8ee1be9bc7db27c8329c5d4cc66f97913
ladimiroING <ladimiro1010yahoo.com>
2022-11-04 22:02:51 -0500

    Estableciendo conexion a la base de datos y migracion del entityframework

---
+++ BackComentarios/AplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using BackComentarios.Controllers.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackComentarios
+{
+    public class AplicationDBContext: DbContext
+    {
+        public DbSet<Comentario> Comentario { get; set; }
+
+        public AplicationDBContext(DbContextOptions<AplicationDBContext> options): base(options)
+        {
+
+        }
+    }
+}
--- BackComentarios/Program.cs
+++ BackComentarios/Program.cs
@@ -1,3 +1,6 @@
+using BackComentarios;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string mySqlConfiguration = builder.Configuration.GetConnectionString("MySQLConnection");
+builder.Services.AddDbContext<AplicationDBContext>(options =>
+                 options.UseMySql(mySqlConfiguration, ServerVersion.AutoDetect(mySqlConfiguration)));
 
 var app = builder.Build();
 
