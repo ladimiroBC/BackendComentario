a45ab8743e95090c9316b37f5fdf0952750aa981
ladimiroING <ladimiro1010yahoo.com>
2022-11-04 20:50:50 -0500

    Cargando back e implementado modelo

---
+++ BackComentarios/Controllers/Models/Comentario.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackComentarios.Controllers.Models
+{
+    public class Comentario
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Titulo { get; set; }
+        [Required]
+        public string? Creador { get; set; }
+        [Required]
+        public string? Texto { get; set; }
+        [Required]
+        public DateTime FechaCreacion { get; set; } 
+
+    }
+}
