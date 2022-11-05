50aeb2bd8a03a6139436a4a3df85a9df03581010
ladimiroING <ladimiro1010yahoo.com>
2022-11-05 17:05:45 -0500

    Implementado solicitudes Http en el ComentarioController

--- BackComentarios/Controllers/ComentarioController.cs
+++ BackComentarios/Controllers/ComentarioController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BackComentarios.Controllers.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +11,115 @@ namespace BackComentarios.Controllers
     [ApiController]
     public class ComentarioController : ControllerBase
     {
+        
+        private readonly AplicationDBContext _dbContext;
+
+        public ComentarioController(AplicationDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+
         // GET: api/<ComentarioController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IActionResult> GetComment()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                var listarComentarios = await _dbContext.Comentario.ToListAsync();
+                return Ok(listarComentarios);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // GET api/<ComentarioController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<IActionResult> CommentDetail(int id)
         {
-            return "value";
+            try
+            {
+                var comentario = await  _dbContext.Comentario.FindAsync(id);
+
+                if(comentario == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(comentario);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // POST api/<ComentarioController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> PostComment([FromBody] Comentario comentario)
         {
+
+            try
+            {
+                _dbContext.Add(comentario);
+                await _dbContext.SaveChangesAsync();
+
+                return Ok(comentario);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        
         }
 
         // PUT api/<ComentarioController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> PutComment(int id, [FromBody] Comentario comentario)
         {
+
+            try
+            {
+                if (id != comentario.Id)
+                {
+                    return BadRequest();
+                }
+
+                _dbContext.Update(comentario);
+                await _dbContext.SaveChangesAsync();
+                return Ok(new {mesaage="Comentario actualizado con exito!"});
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // DELETE api/<ComentarioController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> DeleteComment(int id)
         {
+            try
+            {
+                var comentario = await _dbContext.Comentario.FindAsync(id);
+
+                if(comentario == null)
+                {
+                    return NotFound();
+                }
+
+                _dbContext.Comentario.Remove(comentario);
+                await _dbContext.SaveChangesAsync();
+                return Ok(new { mesaage = "Comentario eliminado con exito!" });
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
         }
     }
 }
